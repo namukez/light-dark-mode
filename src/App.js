9cac6897ae68d82b83fe089ac97561c70cf780aa
namukez <emonname@hotmail.com>
2022-12-20 20:52:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Content from './components/Content';
+import Title from './components/Title';
+import { createContext,useState} from 'react';
+
+export const ThemeContext = createContext()
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [theme,setTheme] = useState("light")
+  return(
+    <ThemeContext.Provider value={{theme,setTheme}}>
+      <div>
+        <Title/>
+        <Content/>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
